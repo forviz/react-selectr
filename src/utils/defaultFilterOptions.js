4dbdd57caae8b7d658efcb35f8fc64a8b941aaa1
Mondit Thumniramon <mondit.thum@gmail.com>
2017-03-05 03:15:05 +0700

    Change Logic to use filterOption in defaultFilterOptions instead

--- src/utils/defaultFilterOptions.js
+++ src/utils/defaultFilterOptions.js
@@ -1,32 +1,28 @@
+import _ from 'lodash';
+
 const createRegExp = str => str;
 // regex pattern '(cat)+\\s?(mat)|(mat)+\\s?(cat)';
-
 
  /*
    isContain function to check searchValue contain in option or not ?
    Note*
      all of searchValue seperated by space (' ') should be contained
  */
-const isContain = (option, searchValue, filterOption = undefined) => {
+const isContain = (option, searchValue) => {
   const searchCriterias = searchValue.split(' '); // split by space
-  let found = true;
-  for (const i in searchCriterias) {
-    const searchCriteria = searchCriterias[i];
-    if (filterOption) {
-      found = filterOption(option, searchValue);
-    } else {
-      const regex = new RegExp(createRegExp(searchCriteria), 'gi');
-      found = regex.test(option.label);
-    }
-    if (!found) return false;
-  }
-  return true;
+  const found = _.every(searchCriterias, (searchCriteria) => {
+    const regex = new RegExp(createRegExp(searchCriteria), 'gi');
+    return regex.test(option.label);
+  });
+  return found;
 };
 
 /*
- * filter options by searchValue then return
+ * filter options by searchValue
  */
-const defaultFilterOptions = (options, searchValue, filterOption) =>
-  options.filter(option => isContain(option, searchValue, filterOption));
+const defaultFilterOptions = (options, searchValue, filterOption = undefined) => {
+  const filterOptionFunc = filterOption || isContain;
+  return options.filter(option => filterOptionFunc(option, searchValue));
+};
 
 export default defaultFilterOptions;
